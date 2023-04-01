9dcf77c8d310931b6ca4f54c3bf4429b6560ad41
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2023-04-01 22:59:41 +0300

    Added mobile menu script

--- source/js/modules/mob-menu/toggle-btn.js
+++ source/js/modules/mob-menu/toggle-btn.js
@@ -1,12 +1,12 @@
-const menuWrapper = document.querySelector(".header__overlay");
-const menuButton = document.querySelector(".menu-toggle");
+const menuWrapper = document.querySelector('.header__overlay');
+const menuButton = document.querySelector('.menu-toggle');
 
 function initMobMenu() {
-  menuButton.addEventListener("click", () => {
-    if (menuWrapper.classList.contains("header__overlay-open")) {
-      menuWrapper.classList.remove("header__overlay-open");
+  menuButton.addEventListener('click', () => {
+    if (menuWrapper.classList.contains('header__overlay-open')) {
+      menuWrapper.classList.remove('header__overlay-open');
     } else {
-      menuWrapper.classList.add("header__overlay-open");
+      menuWrapper.classList.add('header__overlay-open');
     }
   });
 }
